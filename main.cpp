873cbed82257e7673b502b57716bc2677661d5aa
Noah Bruns <noah@pop-os.localdomain>
2020-05-04 11:33:31 +0200

    testcases

--- main.cpp
+++ main.cpp
@@ -2,42 +2,75 @@
 #include <omp.h>
 //#include <stdint.h>
 #include "stdio.h"
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "termcolor.hpp"
+using namespace termcolor;
+#include <set>
+
+void runtest(string name);
 
 int main(int argc, char *argv[]){
-	CoarseList<int> *list; // Create an object of MyClass
-	list = new CoarseList<int>();
-	cout << "add 10: " << list->add(10) << endl;
-	cout << "contains 11: " << list->contains(11) << endl;
-	cout << "add 11: " << list->add(11) << endl;
-	cout << "add 11: " << list->add(11) << endl;
-	cout << "contains 11: " << list->contains(11) << endl;
-	cout << "remove 10: " << list->remove(10) << endl;
-	cout << "remove 10: " << list->remove(10) << endl;
-	cout << "remove 11: " << list->remove(11) << endl;
-	cout << "contain 11: " << list->contains(11) << endl;
+	runtest("testcases/basic.txt");
 
-#pragma omp parallel
-	{
-		printf("Key: %u\n", list->head->next->key);
-		printf("item: %i\n", list->head->next->item);
-		printf("Key: %u\n", list->head->key);
-
-		int NCPU, tid, NPR, NTHR;
-		/* get the total number of CPUs/cores available for OpenMP */
-		NCPU = omp_get_num_procs();
-		/* get the current thread ID in the parallel region */
-		tid = omp_get_thread_num();
-		/* get the total number of threads available in this parallel region */
-		NPR = omp_get_num_threads();
-		/* get the total number of threads requested */
-		NTHR = omp_get_max_threads();
-		/* only execute this on the master thread! */
-		if (tid == 0) {
-			printf("%i : NCPU\t= %i\n", tid, NCPU);
-			printf("%i : NTHR\t= %i\n", tid, NTHR);
-			printf("%i : NPR\t= %i\n", tid, NPR);
-		}
-		printf("%i : hello multicore user! I am thread %i out of %i\n", tid, tid, NPR);
-	}
 	return 0;
+}
+
+void runtest(string name) {
+	ifstream file(name);
+	string line;
+	vector<vector<int>> cases;
+	set<int> valid;
+
+	// Load Test cases
+	if (file.is_open())
+	{
+		while ( getline (file, line) )
+		{
+			vector<int> x;
+			string segment;
+			stringstream l(line);
+
+			while(getline(l, segment, ' '))
+			{
+				auto t = stoi(segment);
+				x.push_back(t);
+				valid.insert(t);
+			}
+
+			cases.push_back(x);
+		}
+
+		file.close();
+	}
+	else {
+		cout << "Unable to open file";
+		return;
+	}
+
+	// Run test Cases
+	CoarseList<int> list;
+
+	cout<<"Running: "<<cases.size()<<" parallel cases"<<endl;
+	#pragma omp parallel
+	#pragma omp for
+	for (const auto &i : cases) {
+		for (const auto &j : i) {
+			list.add(j);
+		}
+	}
+
+	// Compare to Valid
+	bool correct = true;
+	for (const auto &i : valid) {
+		if (!list.contains(i)) {
+			cout<<red<<"Error: "<<reset<<i<<" not in list"<<endl;
+			correct = false;
+		}
+	}
+
+	if (correct) {
+		cout<<green<<"Test succeeded"<<reset<<endl;
+	}
 }
