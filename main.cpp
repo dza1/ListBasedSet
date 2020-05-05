30bd14e0dd6a0c1c36623a4532a1d7e88fa7a5b2
Noah Bruns <noah@pop-os.localdomain>
2020-05-05 11:25:41 +0200

    seperated add and remove testcases

--- main.cpp
+++ main.cpp
@@ -9,19 +9,23 @@
 using namespace termcolor;
 #include <set>
 
-void runtest(string name, SetList<int> &list);
+void runtest(string name, SetList<int> *list);
 
 int main(int argc, char *argv[]){
 
-	CoarseList<int> list;
+	SetList<int> *list;
+	
+	list = new CoarseList<int>();
 	runtest("testcases/basic.txt", list);
-	//runtest("testcases/remove.txt", list);
+	runtest("testcases/remove.txt", list);
+
+	list = new CoarseList<int>();
 	runtest("testcases/large.txt", list);
 
 	return 0;
 }
 
-void runtest(string name, SetList<int> &list) {
+void runtest(string name, SetList<int> *list) {
 	ifstream file(name);
 	string line;
 	vector<vector<int>> cases;
@@ -68,9 +72,9 @@ void runtest(string name, SetList<int> &list) {
 	for (auto it = cases.begin(); it != cases.end(); it++) {
 		for (const auto &j : *it) {
 			if (j < 0)
-				list.remove(-j);
+				list->remove(-j);
 			else if (j > 0)
-				list.add(j);
+				list->add(j);
 			else
 				throw new invalid_argument("0 not allowed");
 		}
@@ -83,7 +87,7 @@ void runtest(string name, SetList<int> &list) {
 	// Compare to Valid
 	bool correct = true;
 	for (const auto &i : valid) {
-		if (!list.contains(i)) {
+		if (!list->contains(i)) {
 			cout<<red<<"Error: "<<reset<<i<<" not in list"<<endl;
 			correct = false;
 		}
